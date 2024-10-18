52188455b3cd27d85393f42307f8ae7a28914383
Mickael <coquerelle.m30@gmail.com>
2024-10-18 20:58:43 +0200

    Correction orthographe sur le main

--- main.sh
+++ main.sh
@@ -9,11 +9,11 @@
 # on choisit dans nos regles de nommages d'identifier chaque variable avec comme première lettre son type de donnée pour stocker l'information le long du script et se répérer.
 # bBooleen, dDictionnaire, sString, cCharact, iInteger, fFloat ...
 
-#------------------------------------ #
-# Contrôles de conformités de l'input #
-#------------------------------------ #
+#-------------------------------------- #
+# Contrôles des conformités sur l'input #
+#-------------------------------------- #
 
-Rep="" # J'initialise mon Rep à null dans l'hypothèse ou la conjonction de mes vérifications préliminaires renverrais false.
+Rep="" # J'initialise mon Rep à null dans l'hypothèse ou la conjonction de mes vérifications renverrait false.
 
 # Vérification des conditions en une seule instruction if
 if [ -z "$1" ] || [ ! -f "$1" ] || [ -d "$1" ] || [ ! -s "$1" ]; then
@@ -29,7 +29,6 @@ if [ -z "$1" ] || [ ! -f "$1" ] || [ -d "$1" ] || [ ! -s "$1" ]; then
     exit 1
 else
     # Si toutes les conditions sont satisfaites, on execute notre Script Python et ca bombarde ;) ...
-    
     echo "Nos controles préliminaires sont terminés, élaboration du rapport en cours ..."
     
     python3 NGS.py "$1"
