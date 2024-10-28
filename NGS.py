3fd308e9eaa82069858ed48adff5d55ddf9911a9
Homero Sanchez <homero.sanchez-moreira@etu.umontpellier.fr>
2024-10-28 12:40:53 +0100

    ajout de la colonne décodage au tableau des flags

--- NGS.py
+++ NGS.py
@@ -35,7 +35,8 @@ def Dico_flags(l_flags):
             d_flags[i_flag] = 1 #si la clé n'existe pas on la crée et on lui donne la valeur 1
     return d_flags
 
-print(Dico_flags(liste_flags(sys.argv[1]))) # afficher le dictionnaire 
+#print(Dico_flags(liste_flags(sys.argv[1]))) # afficher le dictionnaire 
+
 
 
 ###################
@@ -44,10 +45,69 @@ print(Dico_flags(liste_flags(sys.argv[1]))) # afficher le dictionnaire
 
 #On va élaborer un dictionnaire de correspondances des flags établir une correspondance pour pouvoir 
 
-#création d'un tableau avec le dictionnaire retourner par Di	co_flags
+#création d'un tableau avec le dictionnaire retourner par Dico_flags
 d_flags =Dico_flags(liste_flags(sys.argv[1]))
 #Créer un DataFrame à partir du dictionnaire
 t_flags = pd.DataFrame(list(d_flags.items()), columns=['Flag', 'nb de fois présent'])
 #Afficher le tableau
+#print(t_flags)
+
+
+###################
+# MICKAEL 19/10/2024
+###################
+
+# Dictionnaire qui stocke des bits comme clés et des commentaires comme valeurs
+d_Binary_sam = {
+    1: "Read apparié.",
+    2: "Segment apparié correctement selon les critères de l'aligneur.",
+    4: "Segment particulier non aligné.",
+    8: "Segment complémentaire aligné sur le brin négatif.",
+    16: "Segment aligné sur le brin négatif.",
+    32: "L'autre read est aligné en réverse sur le brin positif.",
+    64: "Il s'agit du premier read d'une paire sur le brin positif (5'->3').",
+    128: "Il s'agit du second read d'une paire sur le brin négatif (5' -> 3').",
+    256: "Alignement secondaire (non spécifique, alignement multiple).",
+    512: "Read qui n'a pas passé les filtres de qualité.",
+    1024: "Duplication due à la PCR ou au processus optique.",
+    2048: "Alignement supplémentaire (non spécifique, alignement multiple).",
+}
+
+# Fonction pour décoder la valeur d'un flag en affichant les commentaires correspondants
+def decodage_flags(valeur_du_flag):
+    l_synthese = []
+
+    # Chaque bit représente un flag spécifique et son commentaire associé
+    for i_bit, s_commentaire in d_Binary_sam.items():
+        if valeur_du_flag & i_bit : # En gros ici on vérification l'activation ou non de chaque bit pour la valeur du flag
+            l_synthese.append(f"- {s_commentaire}")
+
+    return "\n".join(l_synthese)
+    
+
+#i_test = 99
+#print(decodage_flags(i_test))
+
+####################
+#HOMERO 28/10/2024#
+####################
+
+#création d'un  dictionnaire retourner par Dico_flags
+d_flags =Dico_flags(liste_flags(sys.argv[1]))
+
+#ajout a ce dictionnaire les commmentaire de decodate_flags
+
+for i_flag in d_flags:
+    l_decodage = decodage_flags(int(i_flag)) #création d'une liste avec les commentaire correspondant a chaque flag 
+    i_nb_fois_present = d_flags[i_flag] #recupere la valeur du nombre de flag present associe a chaque flag 
+    d_flags[i_flag] = [i_nb_fois_present, l_decodage ] #associe a chaque flag une liste avec le nombre de fois que le flag est present et une liste avec les commentaires correspondant au flag
+
+# Conversion du dictionnaire en une liste de tuples [(clé, valeur1, valeur2), ...]
+data = [(cle, valeurs[0], valeurs[1]) for cle, valeurs in d_flags.items()]
+
+# Création du DataFrame avec les colonnes spécifiées
+t_flags = pd.DataFrame(data, columns=['Flag', 'nb de fois présent', 'decodage'])
+
+# Affichage du DataFrame
 print(t_flags)
 
